826700b68918c54b6cb7b42f96586f13fb2d31b2
Prathikskha Jain <prathiksha.jain@iast-software.com>
2026-08-19 18:19:44 +0530

    Prepare portfolio for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// IMPORTANT: if you deploy to USERNAME.github.io/REPO-NAME (a project site),
-// set base to "/REPO-NAME/". If you deploy to USERNAME.github.io (a user site),
-// set base to "/".
 export default defineConfig({
   plugins: [react()],
   base: "/",
